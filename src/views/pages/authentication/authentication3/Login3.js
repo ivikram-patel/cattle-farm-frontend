c5f304521a3b109ef1efc57298fa43a93fe874cb
Vikram Patel <ivikrampatel1@gmail.com>
2024-01-29 15:28:54 +0530

    Remove unnecessary code

--- src/views/pages/authentication/authentication3/Login3.js
+++ src/views/pages/authentication/authentication3/Login3.js
@@ -1,14 +1,14 @@
 import { Link } from 'react-router-dom';
 
 // material-ui
-import { useTheme } from '@mui/material/styles';
-import { Divider, Grid, Stack, Typography, useMediaQuery } from '@mui/material';
+// import { useTheme } from '@mui/material/styles';
+import { Grid, Typography } from '@mui/material';
 
 // project imports
 import AuthWrapper1 from '../AuthWrapper1';
 import AuthCardWrapper from '../AuthCardWrapper';
 import AuthLogin from '../auth-forms/AuthLogin';
-import Logo from 'ui-component/Logo';
+// import Logo from 'ui-component/Logo';
 import AuthFooter from 'ui-component/cards/AuthFooter';
 
 // assets
@@ -16,8 +16,8 @@ import AuthFooter from 'ui-component/cards/AuthFooter';
 // ================================|| AUTH3 - LOGIN ||================================ //
 
 const Login = () => {
-  const theme = useTheme();
-  const matchDownSM = useMediaQuery(theme.breakpoints.down('md'));
+  // const theme = useTheme();
+  // const matchDownSM = useMediaQuery(theme.breakpoints.down('md'));
 
   return (
     <AuthWrapper1>
@@ -27,12 +27,12 @@ const Login = () => {
             <Grid item sx={{ m: { xs: 1, sm: 3 }, mb: 0 }}>
               <AuthCardWrapper>
                 <Grid container spacing={2} alignItems="center" justifyContent="center">
-                  <Grid item sx={{ mb: 3 }}>
+                  {/* <Grid item sx={{ mb: 3 }}>
                     <Link to="#">
                       <Logo />
                     </Link>
-                  </Grid>
-                  <Grid item xs={12}>
+                  </Grid> */}
+                  {/* <Grid item xs={12}>
                     <Grid container direction={matchDownSM ? 'column-reverse' : 'row'} alignItems="center" justifyContent="center">
                       <Grid item>
                         <Stack alignItems="center" justifyContent="center" spacing={1}>
@@ -45,16 +45,19 @@ const Login = () => {
                         </Stack>
                       </Grid>
                     </Grid>
-                  </Grid>
-                  <Grid item xs={12}>
+                  </Grid> */}
+                  {/* <Grid item xs={12}>
                     <AuthLogin />
                   </Grid>
                   <Grid item xs={12}>
                     <Divider />
+                  </Grid> */}
+                  <Grid item xs={12}>
+                    <AuthLogin />
                   </Grid>
                   <Grid item xs={12}>
                     <Grid item container direction="column" alignItems="center" xs={12}>
-                      <Typography component={Link} to="/pages/register/register3" variant="subtitle1" sx={{ textDecoration: 'none' }}>
+                      <Typography component={Link} to="/register" variant="subtitle1" sx={{ textDecoration: 'none' }}>
                         Don&apos;t have an account?
                       </Typography>
                     </Grid>
