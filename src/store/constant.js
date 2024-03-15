ac26e5ec4dcbcab8aaffa6f2cf497179b579b8df
Vikram Patel <143927731+ivikram-patel@users.noreply.github.com>
2024-03-15 23:44:05 +0530

    Update constant.js in Main branch
    after change url in Development branch now change in Main branch

--- src/store/constant.js
+++ src/store/constant.js
@@ -2,7 +2,7 @@
 export const gridSpacing = 3;
 export const drawerWidth = 260;
 export const appDrawerWidth = 320;
-export const API_BASE_URL = 'http://localhost:8000/';
+export const API_BASE_URL = 'http://brahmaani-cattle-farm.infinityfreeapp.com/';
 
 export const ADD_DETAILS = [
   { label: 'Doctor', value: 1 },
